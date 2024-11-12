dee64dfa7fa9ffc1511fec4f7e47754ac65c7ed3
serhii <mynameisserzheo@gmail.com>
2024-11-12 21:58:43 +0200

    Add credit parser

---
+++ TowerDefense/functions/creditParse.py
@@ -0,0 +1,12 @@
+def parse(file_name):
+    file = open(file_name, "r")
+    text_info = []
+
+    for line in file:
+        if len(line.strip()) == 0:
+            text_info.append("")
+        else:
+            cur_line = line.strip()
+            text_info.append(cur_line)
+
+    return text_info
