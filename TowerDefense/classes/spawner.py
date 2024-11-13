23fb9b3fe5c3cb080e533f9ff1bca9c97148b135
serhii <mynameisserzheo@gmail.com>
2024-11-13 21:14:46 +0200

    Add spawner class

---
+++ TowerDefense/classes/spawner.py
@@ -0,0 +1,15 @@
+from game_dev.TowerDefense.classes import enemy
+
+
+class Spawner(object):
+    def __init__(self, attributes, enemy_attributes):
+        self.name = attributes[0]
+        self.interval = attributes[3]
+        self.amount = attributes[1]
+        self.timer = self.interval - 0.25
+        self.spawned_enemy = enemy_attributes
+
+    def spawn_enemy(self, spawnpoint, spawn_num, level):
+        enemy_obj = enemy.Enemy(self.spawned_enemy, spawnpoint, spawn_num, level)
+        self.amount -= 1
+        return enemy_obj
